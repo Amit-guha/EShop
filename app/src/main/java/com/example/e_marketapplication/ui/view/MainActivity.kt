5e6d258139ebc9cd372c814617c3b9d93627d047
Amit guha <amitguho95@gmail.com>
2022-01-06 01:29:25 +0600

    Change the color of status bar

--- app/src/main/java/com/example/e_marketapplication/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/e_marketapplication/ui/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.e_marketapplication.ui.view
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.e_marketapplication.R
@@ -8,5 +9,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Chage the color of Statusbar Cololr
+        Statusebar()
+    }
+    //For Satus bar color
+    private fun Statusebar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.statusBarColor = resources.getColor(R.color.black, this.theme)
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = resources.getColor(R.color.black)
+        }
     }
 }
