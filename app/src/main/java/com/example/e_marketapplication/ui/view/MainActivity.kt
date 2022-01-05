21a3d14c79e066c30ccc15e9f153030a82f7326a
Amit guha <amitguho95@gmail.com>
2022-01-06 00:53:28 +0600

    Add all Necesary Dependency

---
+++ app/src/main/java/com/example/e_marketapplication/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e_marketapplication.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.e_marketapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
